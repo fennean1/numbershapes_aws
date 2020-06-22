c93dc397bdffcf451edba6e8e523ba68365ef614
Andrew Fenner <fennean1@gmail.com>
2020-06-22 11:30:09 -0400

    balls

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -49,8 +49,8 @@ export default function ConceptsCarousel(props) {
 
   return (
     <div className="clouds" style = {{display: "flex",flexDirection: 'column'}}>
-<Link style={{ height: 20, textDecoration: 'none',margin: "auto",display: "flex"}} target = "_blank" to={"/NumberCardStock.pdf"}>
-          <Button color = "secondary" variant = "contained"><Typography style = {{height: 20,fontSize: "1.5vw",display: "flex", margin: "auto",padding: 20,fontFamily: "Chalkboard SE"}} variant="h2" component="h2">
+<Link style={{ height: 40, textDecoration: 'none',margin: "auto",display: "flex"}} target = "_blank" to={"/NumberCardStock.pdf"}>
+          <Button color = "secondary" variant = "contained"><Typography style = {{height: 20,fontSize: 20,display: "flex", margin: "auto",padding: 20,fontFamily: "Chalkboard SE"}} variant="h2" component="h2">
 Number Cards
 </Typography></Button>
         </Link>
