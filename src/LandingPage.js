d2b17f491775a20151b1a418e16199ff0b7226ab
Andrew Fenner <fennean1@gmail.com>
2020-06-22 12:26:23 -0400

    more cowbell

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -50,9 +50,8 @@ export default function ConceptsCarousel(props) {
   return (
     <div className="clouds" style = {{display: "flex",flexDirection: 'column'}}>
 <Link target = "_blank" style={{ textDecoration: 'none',margin: "auto",display: "flex"}}  to={"/NumberCardStock.pdf"}>
-          <Button color = "secondary" variant = "contained"><Typography style = {{flex: 1,fontSize: "5.5vw",display: "flex", margin: "auto",padding: 20,fontFamily: "Chalkboard SE"}} variant="h2" component="h2">
-Number Cards
-</Typography></Button>
+          <Button color = "secondary" variant = "contained">
+Number Card</Button>
         </Link>
    <div className = "container">
       <Tabs
