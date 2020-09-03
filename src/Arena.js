fed72f9748c3701f81fcba7b3b5cb4e1109aae48
Andrew Fenner <fennean1@gmail.com>
2020-09-03 18:14:14 -0400

    semi colon fix

--- src/Arena.js
+++ src/Arena.js
@@ -66,7 +66,7 @@ class Arena extends Component {
     let styleType = this.props.fullscreen ? { height: "100vh" } : null;
     return (
       <div>
-        <Feedback projectId="5f183e5515d6510004b665ea" />;
+        <Feedback projectId="5f183e5515d6510004b665ea" />
         <div
           style={styleType}
           ref={(me) => {
