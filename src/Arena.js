39ea0990dff93ad2eacbde99dd162acb4217dc72
Andrew Fenner <fennean1@gmail.com>
2020-09-03 17:58:33 -0400

    feeder

--- src/Arena.js
+++ src/Arena.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "./App.css";
-
+import Feedback from "feeder-react-feedback"; // import Feedback component
+import "feeder-react-feedback/dist/feeder-react-feedback.css"; // import stylesheet
 
 import * as Pixi from "pixi.js";
 
@@ -10,32 +11,34 @@ class Arena extends Component {
     this.app = {};
     this.state = {
       open: false,
-    }
+    };
   }
 
- handleClose() {
-  this.setState({open: false})
-  };
+  handleClose() {
+    this.setState({ open: false });
+  }
 
-
-  componentWillUnmount(){
-    this.app.destroy(true)
+  componentWillUnmount() {
+    this.app.destroy(true);
   }
 
   componentWillMount() {
-    Pixi.settings.RESOLUTION = 2
-    this.app = new Pixi.Application(0,0,{backgroundColor: 0xffffff,antialias: false});
-    this.app.renderer.backgroundColor = 0xffffff
-    this.app.renderer.resolution = 2
-    this.app.renderer.autoDensity = true
+    Pixi.settings.RESOLUTION = 2;
+    this.app = new Pixi.Application(0, 0, {
+      backgroundColor: 0xffffff,
+      antialias: false,
+    });
+    this.app.renderer.backgroundColor = 0xffffff;
+    this.app.renderer.resolution = 2;
+    this.app.renderer.autoDensity = true;
   }
 
-  loadInstructions(){
-    this.setState({open: true})
+  loadInstructions() {
+    this.setState({ open: true });
   }
 
-  goHome(){
-    window.location.assign("http://www.numbershapes.com")
+  goHome() {
+    window.location.assign("http://www.numbershapes.com");
   }
 
   componentDidMount() {
@@ -44,27 +47,33 @@ class Arena extends Component {
     const setup = {
       height: this.gameCanvas.clientHeight,
       width: this.gameCanvas.clientWidth,
-      props: this.props
+      props: this.props,
     };
 
-    this.app.help = () => this.loadInstructions()
-    this.app.goHome = ()=> this.goHome()
+    this.app.help = () => this.loadInstructions();
+    this.app.goHome = () => this.goHome();
 
-    this.app.renderer.resize(this.gameCanvas.clientWidth,this.gameCanvas.clientHeight)
+    this.app.renderer.resize(
+      this.gameCanvas.clientWidth,
+      this.gameCanvas.clientHeight
+    );
 
     this.props.script(this.app, setup);
-   
   }
 
   // Need fullscreen prop
   render() {
     let styleType = this.props.fullscreen ? { height: "100vh" } : null;
     return (
-      <div style = {styleType}
-        ref={me => {
-          this.gameCanvas = me;
-        }}
-      />
+      <div>
+        <Feedback projectId="5f183e5515d6510004b665ea" />;
+        <div
+          style={styleType}
+          ref={(me) => {
+            this.gameCanvas = me;
+          }}
+        />
+      </div>
     );
   }
 }
